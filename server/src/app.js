b8159d332efc2ddac6266d19cd0b7fb61faef972
Robert Quinlan <quinlan96@live.com.au>
2020-03-25 14:00:20 +1000

    Advertiser should come through now

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', indexRouter);
-app.use('/advertisers', advertisersRouter);
+app.use('/api/advertisers', advertisersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
